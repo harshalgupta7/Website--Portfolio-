474e1792d62c33a39fb5896fbbf312749cdaca16
harshalgupta7 <writetoharshalgupta@gmail.com>
2024-05-19 21:34:19 +0530

    ini
    ini

---
+++ index.php
@@ -0,0 +1,59 @@
+<!DOCTYPE html>
+<html lang="en">
+<?php header("Cache-Control: no-cache, must-revalidate"); ?>
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>My Website</title>
+    <link rel="stylesheet" href="styles.css">
+</head>
+
+<body>
+    <header>
+        <div class="nav_bar">
+            <div></div>
+            <div class="menu">
+                <a href="about.php" class="menu-item">About Me</a>
+                <a href="projects.php" class="menu-item">Projects</a>
+                <a href="resume.php" class="menu-item">Resume</a>
+                <a href="contact.php" class="menu-item">Contact</a>
+            </div>
+        </div>
+    </header>
+
+    <main>
+        <section id="home">
+            <div><h1>Hello <> <br>
+            I'm Harshal </h1>
+            <p>I'm a Backend Software Engineer</p>
+            </div>
+            <div>
+            <span><img src="character.png"> </span>
+            </div>
+        </section>
+
+
+        <section id="about">
+            <h2>About Us</h2>
+            <p>Information about us.</p>
+        </section>
+
+        <section id="services">
+            <h2>Our Services</h2>
+            <p>Details of services offered.</p>
+        </section>
+
+        <section id="contact">
+            <h2>Contact Us</h2>
+        </section>
+    </main>
+
+    <footer>
+        <p>&copy; 2024 Harshal Gupta</p>
+    </footer>
+
+    <script src="script.js"></script>
+</body>
+
+</html>
